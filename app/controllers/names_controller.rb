8a1a59d7952b2b716619103b2de6ba7cf7b59b41
Sam O'Connell <sjo773@gmail.com>
2015-05-31 13:51:39 -0500

    demo basic form

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -23,4 +23,13 @@ end
 
 
 
+def form_display
+
+end
+
+def submit_form
+  @message = params[:message]
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ get "/capital_name" => 'names#capital_name'
 
 get "/number_guess/:number" => 'names#number_guess'
 
+
+
+
+get "/form_display" => 'names#form_display'
+post "/submit_form" => 'names#submit_form'
+
 end
