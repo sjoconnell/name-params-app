2e462bceb7bcbac25f1f26b910fe075927917345
Sam O'Connell <sjo773@gmail.com>
2015-05-31 11:46:46 -0500

    made number guessing game a url param

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
 get "/capital_name" => 'names#capital_name'
 
-get "/number_guess" => 'names#number_guess'
+get "/number_guess/:number" => 'names#number_guess'
 
 end
